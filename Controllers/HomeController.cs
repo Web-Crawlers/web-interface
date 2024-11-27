51f76028f8fa22d750475eba02de463124764460
jonathantfogel <35448709+jonathantfogel@users.noreply.github.com>
2024-11-27 11:08:36 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WebCrawlerInterface.Models;
+
+namespace WebCrawlerInterface.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var responses = new List<string>();
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://10.96.0.101:27017/content");
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    responses.Add(reader.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                responses.Add($"No bueno {e.Message}");
+            }
+
+            ViewData["response"] = responses;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
